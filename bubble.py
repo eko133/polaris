17368506a8bddc528c33c53d9767d75bf3a7a572
siaga <sliuweimin@gmail.com>
2017-04-01 15:35:48 +0800

    更加详细的注释
    添加了一些注释

--- bubble.py
+++ bubble.py
@@ -8,19 +8,23 @@ Created on Sat Apr  1 10:06:23 2017
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import savefig
-#从excel中加载excel文件
+#从excel中加载excel文件,目录自行修改
 df = pd.read_excel('G:\Seafile\临时\positive.xlsx')
+#读取数据的所有化合物类，先剔除掉重复项，再将剩下的列举出来
 y=df['class']
 y=y.drop_duplicates()
 y=y.reset_index()
 m=len(y)
 i=0
 specie=0
+#遍历上述操作找到的所有化合物类，分别绘制图谱
 while i<m:
     specie=y.loc[i,'class']
     x=df[df['class']==specie]
     x['normalized']=x['intensity']/x['intensity'].sum()
+    #分别绘图
     plt.figure(i)
+    #设置图片格式
     font = {'family' : 'serif',  
         'color'  : 'black',  
         'weight' : 'normal',  
@@ -31,5 +35,6 @@ while i<m:
     plt.ylabel("DBE",fontdict=font)
     plt.text(1,14,s=specie,fontdict=font)
     plt.scatter(x['C'],x['DBE'],s=1200*x['normalized'])
+    #保存图片
     savefig(specie+'.png',dpi=600)
     i=i+1
