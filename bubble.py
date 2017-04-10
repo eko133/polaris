960a572d70893e3979c6d5da0b89b8fbb742affa
siaga <sliuweimin@gmail.com>
2017-04-10 09:37:34 +0800

    优化散点图中点大小的计算方式

--- bubble.py
+++ bubble.py
@@ -9,11 +9,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 #从excel中加载excel文件,目录自行修改
-df = pd.read_excel(r'G:\Seafile\临时\正离子excel\1.xlsx')
+df = pd.read_excel(r'G:\Seafile\临时\Biodegradation of sulfur-rich oil\负离子excel\2.xlsx')
 #将intensity转换为float类型
 df['intensity']=df['intensity'].astype(float)
 #按ppm筛选所需数据
-df = df[(df.ppm>-1.2) & (df.ppm<1.2)]
+df = df[(df.ppm>-2.0) & (df.ppm<2.0)]
 #读取数据的所有化合物类，先剔除掉重复项，再将剩下的列举出来
 y=df['class']
 y=y.drop_duplicates()
@@ -23,7 +23,7 @@ i=0
 specie=0
 #创建文件保存位置
 script_dir = os.path.dirname(__file__)
-results_dir = os.path.join(script_dir, '正1/')
+results_dir = os.path.join(script_dir, '负2/')
 if not os.path.isdir(results_dir):
     os.makedirs(results_dir)
 #遍历上述操作找到的所有化合物类，分别绘制图谱
@@ -31,6 +31,8 @@ while i<m:
     specie=y.loc[i,'class']
     x=df[df['class']==specie]
     x['normalized']=x['intensity']/x['intensity'].sum()
+    while x['normalized'].max()<100:
+        x['normalized']=2*x['normalized']
     #分别绘图
     plt.figure(i)
     #设置图片格式
@@ -43,7 +45,7 @@ while i<m:
     plt.xlabel("Carbon Number",fontdict=font)
     plt.ylabel("DBE",fontdict=font)
     plt.text(1,14,s=specie,fontdict=font)
-    plt.scatter(x['C'],x['DBE'],s=1200*x['normalized'])
+    plt.scatter(x['C'],x['DBE'],s=x['normalized'],alpha=0.8,edgecolors='white')
     sample_file_name = specie
     #保存图片
     plt.savefig(results_dir + sample_file_name,dpi=600)
