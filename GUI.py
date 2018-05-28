112949932aadc47abf4f5087761a08fb56105904
siaga <sliuweimin@gmail.com>
2018-05-28 15:19:46 +0800

    add parameters for bubble plots

--- GUI.py
+++ GUI.py
@@ -38,16 +38,16 @@ class Compound:
         self.specie=self.specie()
         
     def mw(self):
-        if self.mode=='+':
+        if self.mode==1:
             a=12 * self.c + atomic_mass['N'] *self. n + atomic_mass['S'] *self. s + atomic_mass['O'] * self.o + atomic_mass['H']*self. h - atomic_mass['e']
-        elif self.mode=='-':
+        elif self.mode==2:
             a=12 * self.c + atomic_mass['N'] *self. n + atomic_mass['S'] *self. s + atomic_mass['O'] * self.o + atomic_mass['H']*self. h + atomic_mass['e']
         return a
 
     def realh(self):
-        if self.mode=='+':
+        if self.mode==1:
             b=self.h-1
-        if self.mode=='-':
+        if self.mode==2:
             b=self.h+1
         return b
         
@@ -112,10 +112,11 @@ class ParaDialog(Toplevel):
         Label(row1,text='DBE',width=5).pack(side=LEFT)
         self.cadbe=IntVar()
         Entry(row1, textvariable=self.cadbe,width=5).pack(side=LEFT)
-        
+                
         Label(row1,text='Mode',width=5).pack(side=LEFT)
-        self.camo=StringVar()
-        Entry(row1,textvariable=self.camo,width=5).pack(side=LEFT)
+        self.camo=IntVar()
+        Radiobutton(row1,text='+ESI',variable=self.camo,value=1).pack(side=LEFT)
+        Radiobutton(row1,text='-ESI',variable=self.camo,value=2).pack(side=LEFT)
         
         row2=Frame(self)
         row2.pack()
@@ -134,6 +135,9 @@ class ParaDialog(Toplevel):
         row3=Frame(self)
         row3.pack()
         Button(row3,text='Cancel',command=self.cancel).pack(side=RIGHT)
+        
+        Label(row3, text='\t').pack(side=RIGHT)
+        
         Button(row3,text='OK',command=self.ok).pack(side=RIGHT)
     
     def ok(self):
@@ -146,16 +150,17 @@ class ParaDialog(Toplevel):
 
 class MenuBar(Menu):       
     
-    def __init__(self,parent,bubbleplotframe):
+    def __init__(self,parent,rawdataframe,bubbleplotframe):
         Menu.__init__(self,parent)
         
         fileMenu=Menu(self)
         
         self.bubbleplotframe=bubbleplotframe
+        self.rawdataframe=rawdataframe
         
         self.add_cascade(label='Import', menu=fileMenu)
         fileMenu.add_command(label='From clipboard', command=self.readClipboard)
-        fileMenu.add_command(label='From excel', command=self.readExcel)
+        fileMenu.add_command(label='From file', command=self.readExcel)
         fileMenu.add_command(label='From folder',command=self.readFolder)
         
         self.text_widget=parent.text_widget
@@ -166,16 +171,18 @@ class MenuBar(Menu):
         
         calMenu=Menu(self)
         self.add_cascade(label='Calculate', menu=calMenu)
+        calMenu.add_command(label='Possible formulas', command=self.processData)
         calMenu.add_command(label='Molecular weight calibration', command=self.mwCa)
-        calMenu.add_command(label='Class abundance from excel', command=self.calAbundance)
+        calMenu.add_command(label='Class abundance from file', command=self.calAbundance)
         calMenu.add_command(label='Class abundance from folder', command=self.calAbundanceFile)
-        calMenu.add_command(label='Class DBE abundance from excel', command=self.caldbeAbundance)
+        calMenu.add_command(label='Class DBE abundance from file', command=self.caldbeAbundance)
         calMenu.add_command(label='Class DBE abundance from folder', command=self.caldbeAbundanceFile)
         
         plotMenu=Menu(self)
         self.add_cascade(label='Plot', menu=plotMenu)
-        plotMenu.add_command(label='Bar plot from excel', command=self.barplot)
-        plotMenu.add_command(label='Bubble plot from folder', command=self.bubbleplot)
+        plotMenu.add_command(label='Bar plot from file', command=self.barplot)
+        plotMenu.add_command(label='Bubble plots from file', command=self.bubbleplotfile)
+        plotMenu.add_command(label='Bubble plots from folder', command=self.bubbleplot)
         
         aboutMenu=Menu(self)
         self.add_cascade(label='Help', menu=aboutMenu)
@@ -239,14 +246,51 @@ class MenuBar(Menu):
         inputDialog=ParaDialog()
         self.wait_window(inputDialog)
         return inputDialog.para
-    
+
+    def processData(self):
+        
+        saveExcel=pd.DataFrame()
+        for i in ('measured m/z','m/z','ppm','class','C','H','O','N','S','DBE','intensity'):
+            saveExcel.loc[0,i]=i
+            i+=i
+        count=0
+        self.data = self.data[self.data['S/N']>=int(self.rawdataframe.snEntry.get())]
+        for column in self.data:
+            if column != 'm/z' and column != 'I':
+                del self.data[column]
+        mw_max=self.data['m/z'].max()
+        mw_min=self.data['m/z'].min()
+        for N,O,S in itertools.product(range(int(self.rawdataframe.nEntry.get())+1), range(int(self.rawdataframe.oEntry.get())+1),range(int(self.rawdataframe.sEntry.get())+1)):
+            c_max=int((mw_max-14*N-16*O-32*S)/12)
+            for C in range(1,c_max+1):
+                h_max=int(mw_max)-12*C-14*N-16*O-32*S+1
+                for H in range(1,h_max+1):
+                    molecule=Compound(C,H,N,O,S,self.rawdataframe.modeEntry.get())
+                    if isMolecule(molecule,mw_min):
+                        data_test=self.data[(self.data['m/z']>=(molecule.mw-mass_tolerance)) & (self.data['m/z']<=(molecule.mw+mass_tolerance))]
+                        if not data_test.empty:
+                            molecule.intensity = data_test['I'].max()
+                            data_test = data_test[data_test['I']==molecule.intensity]
+                            data_test = data_test['m/z'].tolist()
+                            molecule.memw = data_test[0]
+                            molecule.ppm = abs(1000000*(molecule.mw-molecule.memw)/molecule.mw)
+                            if molecule.ppm <= float(self.rawdataframe.ppmEntry.get()):
+                                stringTovar={'measured m/z':molecule.memw,'m/z':molecule.mw,'ppm':molecule.ppm,'class':molecule.specie,'C':molecule.c,'H':molecule.realh,'O':molecule.o,'N':molecule.n,'S':molecule.s,'DBE':molecule.dbe,'intensity':molecule.intensity}
+                                for column in saveExcel:
+                                    saveExcel.loc[count,column]=stringTovar[column]
+                                count+=1
+        self.text_widget.delete('1.0',END)
+        self.text_widget.insert(END,saveExcel)
+        excelSave(saveExcel)
+
+
     def mwCa(self):
         self.setPara()
         capath=filedialog.asksaveasfilename(defaultextension='ref')
         caref=open(capath,'a')
-        if self.camo=='+':
+        if self.camo==1:
             for cac in range(self.cacstart,self.cacstop):
-                camolecule=Compound(cac,2*cac+3+self.can-2*self.cadbe,self.can,self.cao,self.cas,'+')
+                camolecule=Compound(cac,2*cac+3+self.can-2*self.cadbe,self.can,self.cao,self.cas,1)
                 caformula='C'+str(cac)+'H'+str(camolecule.h)
                 if not self.can==0:
                     caformula=caformula+'N'+str(camolecule.n)
@@ -257,9 +301,9 @@ class MenuBar(Menu):
                 caref.write(caformula+' '+str(camolecule.mw)+' '+'1+')   
                 caref.write('\n')
                 
-        if self.camo=='-':
+        if self.camo==2:
             for cac in range(self.cacstart,self.cacstop):
-                camolecule=Compound(cac,2*cac+1+self.can-2*self.cadbe,self.can,self.cao,self.cas,'-')
+                camolecule=Compound(cac,2*cac+1+self.can-2*self.cadbe,self.can,self.cao,self.cas,2)
                 caformula='C'+str(cac)+'H'+str(camolecule.h)
                 if not self.can==0:
                     caformula=caformula+'N'+str(camolecule.n)
@@ -331,7 +375,39 @@ class MenuBar(Menu):
             plt.show()
         except:
             messagebox.showerror('Error', 'Please import data first!')
-
+            
+    def bubbleplotfile(self):
+        species=self.bubbleplotframe.bpclass.get()
+        excelName=os.path.split(self.excelName)[1]
+        species=species.split(',')
+        data=self.data
+        data=data[data['DBE']>0]
+        data['intensity']=data['intensity'].astype(float)
+        path=filedialog.askdirectory()
+        for specie in species:
+            data_specie=data[data['class']==specie]
+            sum=data_specie['intensity'].sum()
+            data_specie['normalized']=data_specie['intensity']/sum
+            plt.figure(figsize=(6,5))
+            font = {'family' : 'arial',  
+                    'color'  : 'black',  
+                    'weight' : 'normal',  
+                    'size'   : 20,  
+                    } 
+            plt.axis([int(self.bubbleplotframe.bpcstart.get()),int(self.bubbleplotframe.bpcstop.get()),int(self.bubbleplotframe.bpdbestart.get()),int(self.bubbleplotframe.bpdbestop.get())])
+            plt.xlabel("Carbon Number",fontdict=font)
+            plt.ylabel("DBE",fontdict=font)
+            plt.xticks(fontsize=16,fontname='arial')
+            plt.yticks(np.arange(int(self.bubbleplotframe.bpdbestart.get()),int(self.bubbleplotframe.bpdbestop.get())+1,2),fontsize=16,fontname='arial')
+            if self.bubbleplotframe.bpshowc.get()==0:
+                plt.text(int(self.bubbleplotframe.bpcstart.get())+1,int(self.bubbleplotframe.bpdbestop.get())-3,s=specie,fontdict=font)
+            if self.bubbleplotframe.bpshows.get()==0:
+                plt.text(int(self.bubbleplotframe.bpcstop.get())-5,int(self.bubbleplotframe.bpdbestop.get())-3,s=excelName,fontdict=font)
+            plt.scatter(data_specie['C'],data_specie['DBE'],s=float(self.bubbleplotframe.bpscale.get())*data_specie['normalized'],edgecolors='black',linewidth=0.1)
+            filename=specie+'.png'
+            plt.savefig(os.path.join(path,filename),dpi=1000)
+        messagebox.showinfo("Complete!", "All plots are stored successfully!")
+        
     def bubbleplot(self):
         os.chdir(self.folder_path)
         excelFile=readAllExcel(self.folder_path)
@@ -369,18 +445,17 @@ class MenuBar(Menu):
                 path=self.folder_path+"\\"+specie
                 filename=excelName+'.png'
                 plt.savefig(os.path.join(path,filename),dpi=600)
-        messagebox.showinfo("Complete!", "All plots are stored in the same folder with the excel files")
+        messagebox.showinfo("Complete!", "All plots are stored in the same folder with excels")
 
     def aboutMessage(self):
         messagebox.showinfo(title='About', message='FT–ICR MS Data Handler\nLicensed under the terms of the Apache License 2.0\n\nDeveloped and maintained by Weimin Liu\n\nFor bug reports and feature requests, please go to my Github website')
         
 class RawDataFrame:
     
-    def __init__(self,parent,menubar):
+    def __init__(self,parent):
         self.frame=Frame(parent)
         self.frame.pack()
         
-        self.menubar=menubar
         
         Label(self.frame, text='RAW DATA',width=10).pack(side=LEFT)
                 
@@ -409,58 +484,17 @@ class RawDataFrame:
         self.sEntry.insert(END,'5')
         self.sEntry.pack(side=LEFT)
         
-        Label(self.frame, text='ESI mode(+,-)').pack(side=LEFT)
-        self.modeEntry=Entry(self.frame,width=3)
-        self.modeEntry.insert(END,'+')
-        self.modeEntry.pack(side=LEFT)
-        
-        self.processButton=Button(self.frame, text='Go!', command=self.processData)
-        self.processButton.pack(side=LEFT)
-        
-        self.text_widget=parent.text_widget
-                
-    def processData(self):
-        
-        saveExcel=pd.DataFrame()
-        for i in ('measured m/z','m/z','ppm','class','C','H','O','N','S','DBE','intensity'):
-            saveExcel.loc[0,i]=i
-            i+=i
-        count=0
-        self.data=self.menubar.data
-        self.data = self.data[self.data['S/N']>=int(self.snEntry.get())]
-        for column in self.data:
-            if column != 'm/z' and column != 'I':
-                del self.data[column]
-        mw_max=self.data['m/z'].max()
-        mw_min=self.data['m/z'].min()
-        for N,O,S in itertools.product(range(int(self.nEntry.get())+1), range(int(self.oEntry.get())+1),range(int(self.sEntry.get())+1)):
-            c_max=int((mw_max-14*N-16*O-32*S)/12)
-            for C in range(1,c_max+1):
-                h_max=int(mw_max)-12*C-14*N-16*O-32*S+1
-                for H in range(1,h_max+1):
-                    molecule=Compound(C,H,N,O,S,self.modeEntry.get())
-                    if isMolecule(molecule,mw_min):
-                        data_test=self.data[(self.data['m/z']>=(molecule.mw-mass_tolerance)) & (self.data['m/z']<=(molecule.mw+mass_tolerance))]
-                        if not data_test.empty:
-                            molecule.intensity = data_test['I'].max()
-                            data_test = data_test[data_test['I']==molecule.intensity]
-                            data_test = data_test['m/z'].tolist()
-                            molecule.memw = data_test[0]
-                            molecule.ppm = abs(1000000*(molecule.mw-molecule.memw)/molecule.mw)
-                            if molecule.ppm <= float(self.ppmEntry.get()):
-                                stringTovar={'measured m/z':molecule.memw,'m/z':molecule.mw,'ppm':molecule.ppm,'class':molecule.specie,'C':molecule.c,'H':molecule.realh,'O':molecule.o,'N':molecule.n,'S':molecule.s,'DBE':molecule.dbe,'intensity':molecule.intensity}
-                                for column in saveExcel:
-                                    saveExcel.loc[count,column]=stringTovar[column]
-                                count+=1
-        self.text_widget.delete('1.0',END)
-        self.text_widget.insert(END,saveExcel)
-        excelSave(saveExcel)
 
-class DataFrame:
-    
-    def __init__(self,parent,menubar):
-        self.frame=Frame(parent)
-        self.frame.pack()
+        Label(self.frame, text='Source',width=5).pack(side=LEFT)
+
+        self.modeEntry=IntVar()
+        
+        Radiobutton(self.frame,text='+ESI',variable=self.modeEntry,value=1).pack(side=LEFT)
+        Radiobutton(self.frame,text='-ESI',variable=self.modeEntry,value=2).pack(side=LEFT)
+        Radiobutton(self.frame,text='+APPI',variable=self.modeEntry,value=3).pack(side=LEFT)
+        Radiobutton(self.frame,text='-APPI',variable=self.modeEntry,value=4).pack(side=LEFT)
+
+                        
         
 class BubblePlotFrame:
     
@@ -520,15 +554,14 @@ class App(Tk):
         self.text_widget = Text(self)
         self.text_widget.pack()
         
+        rawdataframe =RawDataFrame(self)
         bubbleplotframe=BubblePlotFrame(self)
 
-        menubar = MenuBar(self,bubbleplotframe)
+        menubar = MenuBar(self,rawdataframe,bubbleplotframe)
         
         self.config(menu=menubar)
         
         
-        rawdataframe =RawDataFrame(self,menubar)
-        dataframe=DataFrame(self,menubar)
 
         
 if __name__ == '__main__':
