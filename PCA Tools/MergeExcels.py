0401e98fc4b18bf7d01255711106ce4e6766f706
eko133 <sliuweimin@gmail.com>
2020-01-20 17:23:21 +0100

    introducing multi threading

--- PCA Tools/MergeExcels.py
+++ PCA Tools/MergeExcels.py
@@ -6,6 +6,10 @@ os.chdir('/Users/siaga/Desktop/NFT')
 files=os.listdir('/Users/siaga/Desktop/NFT')
 for excel in files:
     raw=pd.read_excel(excel)
+    raw=raw.dropna(axis=1,how='all')
+    raw=raw.dropna()
+    raw['formula']='C'+raw['C'].astype(int).astype(str)+'-H'+raw['H'].astype(int).astype(str)+'-O'+raw['O'].astype(int).astype(str)+'-N'+raw['N'].astype(int).astype(str)
+    raw['m/z']=raw['m/z'].astype(str) + ',' + raw['formula']
     excel=excel.replace('.xlsx','')
     excel=excel.replace('-','_')
     for column in raw:
@@ -13,6 +17,6 @@ for excel in files:
             del raw[column]
     raw['normalized']=raw['intensity']/raw['intensity'].sum()
     del raw['intensity']
-    raw=raw.rename(columns={'m/z':'mass%s'%excel,'normalized':excel})
+    raw=raw.rename(columns={'m/z':'mass%s'%excel,'normalized':excel,'formula':'formula%s'%excel})
     data=pd.concat([data,raw],axis=1,sort=False)
 data.to_excel('/Users/siaga/Desktop/processedData.xlsx')
