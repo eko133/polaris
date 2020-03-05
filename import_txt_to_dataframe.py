560d84bd7be9044eb38942e6e0b4dbadffc701f2
samuel <sliuweimin@gmail.com>
2020-03-05 11:14:52 +0100

    clean branch

---
+++ import_txt_to_dataframe.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+data = pd.read_csv('C:\Users\siaga\Documents\ExportDA', sep=';', header=None)
+
--- target1340/test.js
+++
@@ -1,4 +0,0 @@
-var chemcalc = require('chemcalc');
-var result = chemcalc.mfFromMonoisotopicMass(1301.314833,{'mfRange':'C1-200H1-200N0-5O0-10S0-10','maxUnsaturation':'10','useUnsaturation':'true','integerUnsaturation':'true','massRange':'0.005'});
-console.log(result)
-
